4c0f0dcc5527c4684e382e3a853946db91edd888
trailcode <trailcode@gmail.com>
2018-11-04 20:08:29 -0700

    Start of ColorVertexShader

---
+++ inc/webAsmPlay/shaders/ColorVertexShader.h
@@ -0,0 +1,48 @@
+/**
+ ╭━━━━╮╱╱╱╱╱╱╱╱╱╭╮╱╭━━━╮╱╱╱╱╱╱╭╮
+ ┃╭╮╭╮┃╱╱╱╱╱╱╱╱╱┃┃╱┃╭━╮┃╱╱╱╱╱╱┃┃
+ ╰╯┃┃╰╯╭━╮╭━━╮╭╮┃┃╱┃┃╱╰╯╭━━╮╭━╯┃╭━━╮
+ ╱╱┃┃╱╱┃╭╯┃╭╮┃┣┫┃┃╱┃┃╱╭╮┃╭╮┃┃╭╮┃┃┃━┫
+ ╱╱┃┃╱╱┃┃╱┃╭╮┃┃┃┃╰╮┃╰━╯┃┃╰╯┃┃╰╯┃┃┃━┫
+ ╱╱╰╯╱╱╰╯╱╰╯╰╯╰╯╰━╯╰━━━╯╰━━╯╰━━╯╰━━╯
+ // This software is provided 'as-is', without any express or implied
+ // warranty.  In no event will the authors be held liable for any damages
+ // arising from the use of this software.
+ // Permission is granted to anyone to use this software for any purpose,
+ // including commercial applications, and to alter it and redistribute it
+ // freely, subject to the following restrictions:
+ // 1. The origin of this software must not be misrepresented; you must not
+ //    claim that you wrote the original software. If you use this software
+ //    in a product, an acknowledgment in the product documentation would be
+ //    appreciated but is not required.
+ // 2. Altered source versions must be plainly marked as such, and must not be
+ //    misrepresented as being the original software.
+ // 3. This notice may not be removed or altered from any source distribution.
+
+  \author Matthew Tang
+  \email trailcode@gmail.com
+  \copyright 2018
+*/
+
+#ifndef __WEB_ASM_PLAY_COLOR_VERTEX_SHADER_H__
+#define __WEB_ASM_PLAY_COLOR_VERTEX_SHADER_H__
+
+#include <webAsmPlay/shaders/Shader.h>
+
+class ColorVertexShader : public Shader
+{
+public:
+
+    static void ensureShader();
+
+    static ColorVertexShader * getInstance();
+
+    void bind(const glm::mat4 & MVP, const glm::mat4 & MV, const bool isOutline);
+
+private:
+
+    ColorVertexShader();
+    ~ColorVertexShader();
+};
+
+#endif // __WEB_ASM_PLAY_COLOR_VERTEX_SHADER_H__
